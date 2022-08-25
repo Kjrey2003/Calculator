7e790529401dcbb662361a02c19da415af1c2192
Kjrey2003 <kajanirey@gmail.com>
2022-08-25 10:20:14 -0400

    Added visual changes, made the screen more functional. Calc in the middle

--- stuff.js
+++ stuff.js
@@ -1,22 +1,45 @@
+document.getElementById("clear");
+document.getElementById("output").style.fontSize = '55px';
+
+
+
+
+//create a display and use dom manipulation to put the answers 
 function add(num1, num2){
     sum = num1 + num2;
+    document.getElementById("output").innerHTML = sum;
     return sum; 
+    
 }
 
 function subtract(num1, num2){
     difference = num1 - num2;
+    document.getElementById("output").innerHTML = difference;
     return difference;
 }
 
 function divide(num1, num2){
     quotient = num1 / num2;
+    document.getElementById("output").innerHTML = quotient;
     return quotient;
 }
 
 function multiply(num1, num2){
     product = num1 * num2;
+    document.getElementById("output").innerHTML = product;
     return product;
 }
+
+clear.addEventListener("click", function(){
+  document.getElementById("output").innerHTML = "";
+
+})
+
+
+
+
+
+
 
 function operate(num1, num2, operator) {
     switch (operator) {
